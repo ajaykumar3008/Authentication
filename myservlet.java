8913366d50a28c88195b12c391101b8b146c935d
ajaykumar3008 <128442440+ajaykumar3008@users.noreply.github.com>
2023-08-22 23:24:57 +0530

    Create myservlet.java

---
+++ myservlet.java
@@ -0,0 +1,27 @@
+package booking;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		response.getWriter().write("<html>");
+		response.getWriter().write("<head>");
+		response.getWriter().write("</head>");
+		response.getWriter().write("<body>");
+		response.getWriter().write("<h1 align='center'>WELCOME TO MY SERVLET<h1>");
+		response.getWriter().write("</body>");
+		response.getWriter().write("</html>");
+	}
+
+
+}
