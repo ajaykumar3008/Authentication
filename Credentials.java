1024ce460df5bb9ad75694fd933a7f12bc635675
ajaykumar3008 <128442440+ajaykumar3008@users.noreply.github.com>
2023-08-22 23:22:53 +0530

    Update Credentials.java

--- Credentials.java
+++ Credentials.java
@@ -1,7 +1,8 @@
-package autharization;
+package Validation;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.HashMap;
@@ -9,21 +10,43 @@ import java.util.HashMap;
 public class Credentials {
 
 	HashMap<String, String> hm = new HashMap<>();
+	String user;
+	String password;
+	
+	public Credentials(String user,String pass) {
+		
+		this.user=user;
+		this.password=pass;
+	}
+	
+	public boolean verifyCredentials() {
+		
+		boolean approve=false;
+		if(hm.containsKey(user)) {
+			String real=hm.get(user);
+			if(real.equals(password)) {
+				approve=true;
+			}
+		}
+		
+		return approve;
+		
+	}
 
 	public HashMap<String, String> getCredentials() {
 
 		try {
 
 			Class.forName("org.postgresql.Driver");
-			Connection con = DriverManager.getConnection("jdbc:postgresql://192.168.110.48:5432/plf_training",
-					"plf_training_admin", "pff123");
+			Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/ajay",
+					"postgres", "Maddineni@123");
 
 			Statement st = con.createStatement();
 
-			ResultSet rs = st.executeQuery("select * from ajayEmp");
+			ResultSet rs = st.executeQuery("select * from credentials");
 
 			while (rs.next()) {
-				hm.put(rs.getString("user"), rs.getString("password"));
+				hm.put(rs.getString("userid"), rs.getString("password"));
 			}
 		} catch (Exception e) {
 
@@ -31,5 +54,62 @@ public class Credentials {
 		}
 
 		return hm;
+	}
+
+	public String generateKey() {	
+		
+		return "ajhsgteb123b3g42b";
+	}
+	
+	public void updateTable(String key,String user) {
+		
+		try {
+			
+			
+			Class.forName("org.postgresql.Driver");
+			Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/ajay",
+				"postgres", "Maddineni@123");
+
+			PreparedStatement ps = con.prepareStatement("update credentials set key=? where userid=?");
+			
+			ps.setString(1, key);
+			ps.setString(2, user);
+
+			ps.executeUpdate();
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+			
+	}
+	
+	public boolean checkKey(String key,String user) {
+		
+		boolean access=false;
+		
+		try {
+			
+			
+			Class.forName("org.postgresql.Driver");
+			Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/ajay",
+				"postgres", "Maddineni@123");
+
+			Statement st = con.createStatement();
+			String s="select key from credentials where userid="+user;
+			ResultSet rs=st.executeQuery(s);
+
+			if(rs.getString(3)!=null&&key.equals(rs.getString(3))) {
+				access=true;
+			}
+			
+			con.close();
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		}
+		
+		
+		return access;
 	}
 }
