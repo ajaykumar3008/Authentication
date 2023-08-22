c4b3b120af32e96d16738c791297c6e508bec48b
ajaykumar3008 <128442440+ajaykumar3008@users.noreply.github.com>
2023-08-22 16:54:18 +0530

    Create verification.java

---
+++ verification.java
@@ -0,0 +1,27 @@
+package autharization;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Verification")
+public class Verification extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String userName = request.getHeader("user");
+		String password = request.getHeader("pass");
+
+		Credentials cr = new Credentials();
+		HashMap<String, String> credentials = cr.getCredentials();
+
+	}
+
+}
