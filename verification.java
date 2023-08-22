b09eca5d048fc79d5422a91d14da365a5ea90c1e
ajaykumar3008 <128442440+ajaykumar3008@users.noreply.github.com>
2023-08-22 23:24:08 +0530

    Update verification.java

--- verification.java
+++ verification.java
@@ -1,15 +1,13 @@
-package autharization;
+package Validation;
 
 import java.io.IOException;
-import java.util.HashMap;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/Verification")
 public class Verification extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -19,8 +17,26 @@ public class Verification extends HttpServlet {
 		String userName = request.getHeader("user");
 		String password = request.getHeader("pass");
 
-		Credentials cr = new Credentials();
-		HashMap<String, String> credentials = cr.getCredentials();
+		Credentials cr = new Credentials(userName,password);
+		
+		boolean status=cr.verifyCredentials();
+		
+		if(status==true) {
+			
+			String key=cr.generateKey();
+			cr.updateTable(key, userName);
+			request.setAttribute("key", key);
+			request.setAttribute("user", userName);
+			request.setAttribute("cred", cr);
+			RequestDispatcher rd=request.getRequestDispatcher("/my-servlet");
+			rd.forward(request, response);
+		}
+		else {
+			
+			RequestDispatcher rd=request.getRequestDispatcher("/response.jsp");
+			rd.forward(request, response);
+			
+		}
 
 	}
 
